d4407ab8a4e727b20d7554977d1b83e483cf2f7e
xNikus <comlokoxd@gmail.com>
2022-01-23 20:37:55 +0100

    1.4 stale 2 Generate getter for total variable

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -8,6 +8,10 @@ public class DefaultCountingOutRhymer {
 
     private int total = -1;
 
+    public int getTotal() {
+        return total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
